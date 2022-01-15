9e9588e9ead8a0e3b79c79c2a660b09a3e34c30e
Documenter.jl <documenter@juliadocs.github.io>
2022-01-15 09:23:23 +0000

    build based on 23c1a21

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MonkeyLang","category":"page"},{"location":"#MonkeyLang","page":"Home","title":"MonkeyLang","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MonkeyLang.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MonkeyLang]","category":"page"}]
+}
